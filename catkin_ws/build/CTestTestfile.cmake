4616317f3d7a75f7dcf2103ffc86f47b933ca0d7
CurtisBurke-eng <curtisburke.eng@gmail.com>
2023-03-07 07:03:28 +0000

    Add catkin workspace to project

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/admin_user/KEEL_ROSMaster/catkin_ws/src
+# Build directory: /home/admin_user/KEEL_ROSMaster/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
