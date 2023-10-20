46a6d158c3749241bd3df1e413d1f0247be2b6dc
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-20 14:05:02 -0600

    Change file types & organize folders

--- functions/db_connect.php
+++ functions/db_connect.php
@@ -1,6 +1,6 @@
 <?php
     // connect to the database
-    $conn = mysqli_connect('localhost','web','test','BBDrawTrailers');
+    $conn = mysqli_connect('localhost','web','test','KEEL_Inventory');
 
     // Check Connection
     if(!$conn) {
