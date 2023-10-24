dac1c42e4428a45e5c728e64094502525c69247e
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-24 15:49:29 -0600

    Change from a single conn to a pool

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2'
 
-const conn = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
