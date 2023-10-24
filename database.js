e9e20ced120f8fd183e3488c60ef8da78faa554f
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-24 15:55:32 -0600

    Use hardcoded login instead of env vars

--- database.js
+++ database.js
@@ -1,8 +1,15 @@
 import mysql from 'mysql2'
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    user: process.env.MYSQL_DATABASE
+    // host: process.env.MYSQL_HOST,
+    // user: process.env.MYSQL_USER,
+    // password: process.env.MYSQL_PASSWORD,
+    // database: process.env.MYSQL_DATABASE
+    host: '127.0.0.1',
+    user: 'web',
+    password: 'test',
+    database: 'KEEL_Inventory'
 }).promise()
+
+const [rows] = await pool.query("SELECT * FROM badges;")
+console.log(rows)
