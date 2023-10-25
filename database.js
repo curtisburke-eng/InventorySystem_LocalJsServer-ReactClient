2a759cf350f5c545197125c39d6fc532c74ff859
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-25 10:49:18 -0600

    Configure dotenv

--- database.js
+++ database.js
@@ -1,15 +1,19 @@
 import mysql from 'mysql2'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const pool = mysql.createPool({
-    // host: process.env.MYSQL_HOST,
-    // user: process.env.MYSQL_USER,
-    // password: process.env.MYSQL_PASSWORD,
-    // database: process.env.MYSQL_DATABASE
-    host: '127.0.0.1',
-    user: 'web',
-    password: 'test',
-    database: 'KEEL_Inventory'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
 
-const [rows] = await pool.query("SELECT * FROM badges;")
-console.log(rows)
+async function getBadges() {
+    const [rows] = await pool.query("SELECT * FROM badges;")
+    return rows
+}
+
+const badges = await getBadges()
+console.log(badges)
