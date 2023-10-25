94a1f10e6eed3f4d3e7cade7ec9b55a5c87c3a9f
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-25 12:01:31 -0600

    Create GET reqs and routes for size, color, model

--- app.js
+++ app.js
@@ -13,11 +13,58 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // Get functions from database server
-import {getBadges} from './database.js'
+import {getBadges, getBadgesBySize, getBadgesByColor, getBadgesByModel} from './database.js'
 
-// Get Request Routes
+// --- Get Request Routes ---
+// GET all badges
 app.get("/badges", async (req,res) => {
     const badges = await getBadges()
+    res.send(badges)
+})
+
+// GET badges by size
+app.get("/badges/21", async (req,res) => {
+    const badges = await getBadgesBySize(21)
+    res.send(badges)
+})
+app.get("/badges/22", async (req,res) => {
+    const badges = await getBadgesBySize(22)
+    res.send(badges)
+})
+app.get("/badges/24", async (req,res) => {
+    const badges = await getBadgesBySize(24)
+    res.send(badges)
+})
+
+// GET badges by color
+app.get("/badges/uncoated", async (req,res) => {
+    const badges = await getBadgesByColor("uncoated")
+    res.send(badges)
+})
+app.get("/badges/black", async (req,res) => {
+    const badges = await getBadgesByColor("black")
+    res.send(badges)
+})
+app.get("/badges/red", async (req,res) => {
+    const badges = await getBadgesByColor("red")
+    res.send(badges)
+})
+app.get("/badges/blue", async (req,res) => {
+    const badges = await getBadgesByColor("blue")
+    res.send(badges)
+})
+app.get("/badges/rose", async (req,res) => {
+    const badges = await getBadgesByColor("rose")
+    res.send(badges)
+})
+
+// GET badges by Model
+app.get("/badges/standard", async (req,res) => {
+    const badges = await getBadgesByModel("standard")
+    res.send(badges)
+})
+app.get("/badges/XL", async (req,res) => {
+    const badges = await getBadgesByModel("XL")
     res.send(badges)
 })
 
--- database.js
+++ database.js
@@ -1,8 +1,17 @@
+/*
+*
+*
+*
+*/
+
+// Use SQL library for connecting & querying
 import mysql from 'mysql2'
 
+// Use Enviroment vars for easy updating
 import dotenv from 'dotenv'
 dotenv.config()
 
+// Create a pool of connections - rather than having a different connection used for each query
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -10,10 +19,36 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-async function getBadges() {
+// Create the SQL statements used in the GET requests
+
+export async function getBadges() {                                             // GET all badges
     const [rows] = await pool.query("SELECT * FROM badges;")
     return rows
 }
 
-const badges = await getBadges()
-console.log(badges)
+export async function getBadgesBySize(size) {                                   // GET badges by size
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM badges
+    WHERE size_mm = ?
+    `,[size])
+    return rows
+}
+
+export async function getBadgesByColor(color) {                                 // GET badges by color
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM badges
+    WHERE color = ?
+    `,[color])
+    return rows
+}
+
+export async function getBadgesByModel(model) {                                 // GET badges by color
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM badges
+    WHERE model = ?
+    `,[model])
+    return rows
+}
