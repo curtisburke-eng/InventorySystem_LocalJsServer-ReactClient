35a447c801a49e0ada054f089b59a177e8c4b5b5
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-26 15:41:59 -0600

    Fetch badges from API

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {use, useEffect, useState} from 'react'
+
+
 
 function index() {
+
+  const [badges, setBadges] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:8080/badges").then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log(data)
+        setBadges(data)
+        console.log(badges)
+      }
+
+    )
+  },[])
+
   return (
     <div id="root">
-      index
+      
+      {/* {
+        // for (i = 0; i < badges.length; i++) {
+        //   for (j = 0; j < badges[j].length; j++) {
+        //       document.write(badges[i][j]);
+        //   }
+        // }
+        
+        badges.forEach(item => {
+            console.log(item)
+        })
+      } */}
+
     </div>
   )
 }
