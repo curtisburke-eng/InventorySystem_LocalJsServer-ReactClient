981992644300d05a6ea8ca01b6ebd376654fe126
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-26 15:58:00 -0600

    Replace app with server

--- server/js/server.js
+++ server/js/server.js
@@ -6,8 +6,8 @@
 
 // Use Express to Create a HTTP Server
 import express from 'express'
-const app = express()
-app.use(express.json()) // any json http body will be accepted and passed into the req.body object
+const server = express()
+server.use(express.json()) // any json http body will be accepted and passed into the req.body object
 
 // Use Enviroment vars for easy updating
 import dotenv from 'dotenv'
@@ -18,7 +18,7 @@ import {getBadges, getAllBadges, getBadgesBySize, getBadgesByColor, getBadgesByM
 
 // --- Get Request Routes ---
 // GET all badges
-app.get("/badges", async (req,res) => {
+server.get("/badges", async (req,res) => {
     if (req.headers['content-length'] != null) {                // If there is information in the body of the request
         const {size_mm, color, model} = req.body                // Extract that info
         const badges = await getBadges(size_mm, color, model)   // Use that info for the query
@@ -31,7 +31,7 @@ app.get("/badges", async (req,res) => {
 })
 
 // GET badges by size
-app.get("/badges/bySize", async (req,res) => {
+server.get("/badges/bySize", async (req,res) => {
     const {size_mm} = req.body
     const badges = await getBadgesBySize(size_mm)
     res.status(201).send(badges)
@@ -39,14 +39,14 @@ app.get("/badges/bySize", async (req,res) => {
 })
 
 // GET badges by color
-app.get("/badges/byColor", async (req,res) => {
+server.get("/badges/byColor", async (req,res) => {
     const {color} = req.body
     const badges = await getBadgesByColor(color)
     res.status(201).send(badges)
 })
 
 // GET badges by model
-app.get("/badges/byModel", async (req,res) => {
+server.get("/badges/byModel", async (req,res) => {
     const {model} = req.body
     const badges = await getBadgesByModel(model)
     res.status(201).send(badges)
@@ -56,7 +56,7 @@ app.get("/badges/byModel", async (req,res) => {
 // Update All badges
 
 // Update single badge
-app.put("/badges", async (req,res) => {
+server.put("/badges", async (req,res) => {
     const {size, color, model, count_onHand, count_onOrder} = req.body
     const badges = await updateBadgeQty(size, color, model, count_onHand,count_onOrder)
     res.status(201).send(badges)
@@ -64,11 +64,11 @@ app.put("/badges", async (req,res) => {
 
 
 // Error Handling
-app.use((err, req, res, next) => {
+server.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
   })
 
-app.listen(process.env.PORT,() => {
+server.listen(process.env.PORT,() => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
