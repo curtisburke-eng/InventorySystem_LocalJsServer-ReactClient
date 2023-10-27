9daaed5fa9e619d60f2881869317eadcde96e217
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-27 14:53:03 -0600

    Add Comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
+// Get Library Functions
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+// Get assets & CSS
 import './App.css'
 import keelLogo from './assets/KEEL-logo_white_140x.png'
 
+// Create Main App Component
 function App() {
-
+  // ----- Declare vars -----
   const [badges,setBadges] = useState([])
   const [error,setError] = useState(false)
+
+  // ----- Setup & Run Functions for queries -----
 
   // Get all badges from server (on Startup & Refresh)
   useEffect(()=>{
     ;(async () => {
       setError(false) // Reset error to false
       try{
-        const result = await axios.get('/api/badges')
+        const result = await axios.get('/api/badges') // Query all badges
         setBadges(result.data)
 
       } catch(error) {
@@ -23,21 +27,19 @@ function App() {
     })()
   },[])
 
-
-
-
-  const [count, setCount] = useState(0)
-
+  // ----- Return Markup with loaded data -----
   return (
     <>
+      {/* ----- Display Logo & Title ----- */}
       <div>
         <a href="https://shopkeel.us" target="_blank">
           <img src={keelLogo} className="logo" alt="KEEL Logo" />
         </a>
       </div>
       <h1>KEEL Inventory</h1>
-
       
+      {/* ----- Display Fetched data for Badges ----- */}
+
       { error ? // If there is an error display this message
       <p>Something went wrong fetching the Data!</p>
 
@@ -51,16 +53,6 @@ function App() {
 
       ))}
 
-
-      <h1>Card</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       
     </>
   )
