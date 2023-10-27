1e115696b6e3d703c485443f8ac33f3c01234f09
curtisburke-eng <curtisburke.eng@gmail.com>
2023-10-27 14:46:47 -0600

    Create Get request & badge cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import './App.css'
 import keelLogo from './assets/KEEL-logo_white_140x.png'
 
 function App() {
+
+  const [badges,setBadges] = useState([])
+  const [error,setError] = useState(false)
+
+  // Get all badges from server (on Startup & Refresh)
+  useEffect(()=>{
+    ;(async () => {
+      setError(false) // Reset error to false
+      try{
+        const result = await axios.get('/api/badges')
+        setBadges(result.data)
+
+      } catch(error) {
+        setError(true) 
+      }
+    })()
+  },[])
+
+
+
+
   const [count, setCount] = useState(0)
 
   return (
@@ -12,7 +35,24 @@ function App() {
           <img src={keelLogo} className="logo" alt="KEEL Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>KEEL Inventory</h1>
+
+      
+      { error ? // If there is an error display this message
+      <p>Something went wrong fetching the Data!</p>
+
+      : // Else load badges
+      badges.map((badge) => (
+        <div className="card" key={badge.id}>
+          <h3>Size: {badge.size_mm}</h3>
+          <h3>Model: {badge.model}</h3>
+          <h3>Color: {badge.color}</h3>
+        </div>
+
+      ))}
+
+
+      <h1>Card</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -21,9 +61,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
